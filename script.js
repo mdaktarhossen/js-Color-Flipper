565905ce9a80baf1e6279aa89ae721c1a291be63
Md akter hosen <mdaktarhossen20@gmail.com>
2022-01-19 17:28:53 +0600

    color flipper done

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const colors=["#46B2E0", "#B4161B", "#6AC47E", "#242B2E", "#0D0D0D", "#120E43"]
-const btn=document.getElementById('btn')
-let color=document.querySelector('#color')
+const colors=["#46B2E0", "#B4161B", "#6AC47E", "#242B2E", "#0D0D0D", "#120E43"];
+const btn=document.getElementById('btn');
+let color=document.querySelector('#color');
 
 btn.addEventListener("click",()=>{
-  const randomNumber=Math.floor(Math.random()*colors.length)
+  const randomNumber=Math.floor(Math.random()*colors.length);
   document.body.style.backgroundColor=colors[randomNumber];
-  color.textContent=colors[randomNumber]
+  color.textContent=colors[randomNumber];
 })
